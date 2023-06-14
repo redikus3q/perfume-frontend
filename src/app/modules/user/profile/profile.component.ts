2f00182625340db4ee2e7524e0190e5d4d5ecf41
Alexandru-Daniel Tiganus <alexandu24@gmail.com>
2023-06-14 17:43:11 +0300

    rebuild

--- src/app/modules/user/profile/profile.component.ts
+++ src/app/modules/user/profile/profile.component.ts
@@ -1,31 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  public user: any;
 
-  public user : any;
-  private subscription: Subscription | any;
-
-  constructor(
-    private router: Router,
-    private authService: AuthService
-  ) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    var username = localStorage.getItem("Username");
-    if(username == undefined || username == ""){
-      this.user = undefined;
-    }
-    else {
-      this.user = localStorage.getItem("Username");
-    }
+    this.authService.getUser().subscribe((result) => {
+      this.user = result.username;
+    });
   }
-
 }
